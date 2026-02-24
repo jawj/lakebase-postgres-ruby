bfd31f21fe2cc20d8e984a0dd3ec1ca3a020ee50
George MacKerron <georgemackerron@neon.tech>
2026-02-24 10:14:20 +0000

    Tweaks

--- lib/lakebase/pg_dynamic_password.rb
+++ lib/lakebase/pg_dynamic_password.rb
@@ -1,13 +1,13 @@
 require 'pg'
 
 module CallablePasswordParam
-  # patch pg to support a callable object as the :password key of the config hash
+  # patch pg to support a lambda, Proc, etc. as the :password config key
   def parse_connect_args(*args)
     return super(*args) unless args.last.is_a?(Hash)
 
-    hash_arg = args.pop.transform_keys(&:to_sym)
-    hash_arg[:password] = hash_arg[:password].call if hash_arg[:password].respond_to?(:call)
-    super(*args, hash_arg)
+    hash = args.pop.transform_keys(&:to_sym)
+    hash[:password] = hash[:password].call if hash[:password].respond_to?(:call)
+    super(*args, hash)
   end
 end
 
